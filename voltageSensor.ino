b3d2193014b29d1e436c6ff6985f38207e6ed770
PMARINA <PMARINA@users.noreply.github.com>
2019-03-07 09:08:37 -0500

    Update VoltageSensor.ino using voltage sensors

--- voltageSensor.ino
+++ voltageSensor.ino
@@ -1,43 +1,20 @@
+const int voltage_tap_5v = A5;
+
 void setup() {
  
 Serial.begin(9600);       //begin serial com at 9600 bps (bits)
-pinMode(A5, INPUT);
-pinMode(12, OUTPUT);
-pinMode(13, OUTPUT);      //enable internal pull up resistor to make those pins outputs
+pinMode(voltage_tap_5v, INPUT);
 }
 
 void loop() {
   
-  int input = analogRead(A5);     //input the pin you're reading from. Signal voltage value
+  double input = analogRead(voltage_tap_5v);     //input the pin you're reading from. Signal voltage value
   
   //arduino board ADC reads voltages on a 0-1023 scale for 0-5V
   float voltage = input * (5.0/1023.0);
-  Serial.print(voltage);
-  Serial.print("\t");
-
-  
-  //math to convert our voltage value to a current value      the equation is defined on the flexscada website
-//   int CF = .0224;    // math constant for conversion of sensor
-//   float current = (voltage - 2.5)/.0224;
-//   Serial.println(current);
-
-
-//if(current > 40){                   // if the current gets above certain thresholds then light up two warning lights
-//  digitalWrite(12,HIGH);
-//  
-//}
-//if(current >= 50) {
-//  digitalWrite(13, HIGH);
-//    
-//  }
-  
-}
-
-
-  /*
-  int voltage = 10;
-  int current = 5;
+  double res = 25;
+  voltage = voltage*res;
+  voltage = 0.8386*voltage-0.1781 //R^2=0.9999, intercept = (0,-0.1781), point (0,0) included, not forced
   Serial.println(voltage);
-  Serial.println(current);
-  */
+}
   
